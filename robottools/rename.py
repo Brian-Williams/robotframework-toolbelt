f0810852ad91458ef71f131741bf30105b44c02d
Brian Williams <brian.williams@actifio.com>
2017-08-22 03:00:02 -0400

    Add working rerunrenamedsuites
    Needed to avoid recursion from monkeypatching what was then being
    called with __get__. Don't code when tired kids.

--- robottools/rename.py
+++ robottools/rename.py
@@ -1,5 +1,8 @@
 from robot.api import SuiteVisitor
 from robot.conf import gatherfailed
+from robot.result import ExecutionResult
+from robot.errors import DataError
+from robot.utils import get_error_message
 
 
 class rename(SuiteVisitor):
@@ -53,35 +56,24 @@ class rerunrenamedtests(SuiteVisitor):
         suite.filter(included_tests=tests)
 
 
-# won't work because of critically from root bug
-class RenameThenGatherFailedSuites(resetname, gatherfailed.GatherFailedSuites):
+class RenameThenGatherFailedSuites(gatherfailed.GatherFailedSuites, resetname):
     def start_suite(self, suite):
-        for test in suite.tests:
-            print("test {} {}".format(test, not test.passed))
-
-        if any(not test.passed for test in suite.tests):
-            print("goteeeem, {}".format(suite.longname))
-            print("suites before append {}".format(self.suites))
-            self.suites.append(suite.longname)
-            print("suites after append {}".format(self.suites))
-
-        super(RenameThenGatherFailedSuites, self).start_suite(suite)
+        resetname.start_suite.__get__(self)(suite)
+        gatherfailed.GatherFailedSuites.start_suite.__get__(self)(suite)
 
 
-gatherfailed.GatherFailedSuites = RenameThenGatherFailedSuites
-
-
+# Should be copied exactly from robot/conf/gatherfailed.py
 def gather_failed_suites(output):
     if output.upper() == 'NONE':
         return []
     gatherer = RenameThenGatherFailedSuites()
-
-    from robot.result import ExecutionResult
-    ExecutionResult(output, include_keywords=False).suite.visit(gatherer)
-    if not gatherer.suites:
-        print('All suites passed. {}'.format(gatherer.suites))
-
-    print("return {}".format(gatherer.suites))
+    try:
+        ExecutionResult(output, include_keywords=False).suite.visit(gatherer)
+        if not gatherer.suites:
+            raise DataError('All suites passed.')
+    except:
+        raise DataError("Collecting failed suites from '%s' failed: %s"
+                        % (output, get_error_message()))
     return gatherer.suites
 
 
@@ -90,10 +82,5 @@ class rerunrenamedsuites(SuiteVisitor):
         self.output = output
 
     def start_suite(self, suite):
-        print("running for {}".format(suite))
-        print("Beofre {}...{}".format(suite.tests, suite.suites))
-
         suites = gather_failed_suites(self.output)
-        print("suites {}".format(suites))
         suite.filter(included_suites=suites)
-        print("After {}".format(suite.tests))
