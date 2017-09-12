b3039bc01290d462ea7dd74eee07d3beffe1a234
Brian Williams <brian.williams@actifio.com>
2017-09-12 16:34:43 -0400

    Fix unname hoisting

--- robottools/__init__.py
+++ robottools/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'robottools'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
-from robottools.modifiers.rename import rename, resetname, rerunrenamedtests, rerunrenamedsuites, defaultname  # noqa
+from robottools.modifiers.rename import rename, resetname, rerunrenamedtests, rerunrenamedsuites, unname  # noqa
 from robottools.reporters.testlink import testlink  # noqa
