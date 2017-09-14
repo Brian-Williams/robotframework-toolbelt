c660f5c35b1b5dade5092086e9847a6b9a737839
Brian Williams <brian.williams@actifio.com>
2017-09-14 17:23:58 -0400

    Increment version

--- robottools/__init__.py
+++ robottools/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'robottools'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 from robottools.modifiers.rename import rename, resetname, rerunrenamedtests, rerunrenamedsuites, unname  # noqa
 from robottools.listeners.report_testlink import reporttestlink  # noqa
